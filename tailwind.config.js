668c402471462e54170a45d713bf0e3fbf70ae69
nickblaq <nickookoro200@gmail.com>
2022-04-12 15:46:16 +0100

    big patch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-   plugins: [require("daisyui")],
+   plugins: [],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          primary: "blue",
-          "primary-focus": "mediumblue",
-        },
-      },
-    ],
-  },
   theme: {
     extend: {
       fontFamily: {
