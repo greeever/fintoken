998b8cf57f4f0006ed5194bb3d9faa4db37a6e52
greeever <110606523+greeever@users.noreply.github.com>
2022-08-06 04:42:45 +0100

    danger

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, Script } from 'next/document'
 
 class MyDocument extends Document {
   render() {
@@ -17,6 +17,34 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" class="fb-customerchat"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v14.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+            `,
+          }}
+        />
+
+        <div className="fb-customerchat"
+          attribution="biz_inbox"
+          page_id="103599749122645">
+        </div>
+
       </Html>
     )
   }
