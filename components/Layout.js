841708353a6204cc0aa7df3536b0d602a4a34bc9
greeever <110606523+greeever@users.noreply.github.com>
2022-08-06 02:59:52 +0100

    hide listing

--- components/Layout.js
+++ components/Layout.js
@@ -399,9 +399,9 @@ text-base text-blue-900 underline">
 
 <p className="flex justify-between items-center text-base text-gray-800 dark:text-gray-100">
 
-     $1 /
+     0.05 bnb /
      <div className='flex items-center space-x-1'>
-  <span>0.003</span>
+  <span>10000</span>
   <span className='w-full'>
     <img className='h-5 w-5 md:h-6 md:w-6 ' src='/logo.svg' />
     </span>
@@ -419,17 +419,17 @@ text-base text-blue-900 underline">
   </div>
 
 
-<p className="flex justify-between items-center text-base text-gray-800 dark:text-gray-100 py-4">
+{/* <p className="flex justify-between items-center text-base text-gray-800 dark:text-gray-100 py-4">
 
-     $1 /
+     0.09 bnb /
      <div className='flex items-center space-x-1'>
-  <span>0.009</span>
+  <span></span>
   <span className='w-full'>
     <img className='h-5 w-5 md:h-6 md:w-6 ' src='/logo.svg' />
     </span>
     </div>
      
-     </p>
+     </p> */}
 
   
 </article>
